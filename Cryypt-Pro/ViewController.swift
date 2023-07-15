5bbdff188765e7eb99fce0298f0b9458d35b8908
Burhan <brhngksel@gamil.com>
2023-07-15 23:03:39 +0300

    Initial Commit

---
+++ Cryypt-Pro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cryypt-Pro
+//
+//  Created by Burhan Göksel on 15.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
